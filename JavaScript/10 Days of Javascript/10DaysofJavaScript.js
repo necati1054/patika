ce54e5231b543b2dcbea4d64d767d93f2fe5bad8
Necati <necati.arman1054@gmail.com>
2022-02-07 23:50:19 +0300

    update Day-1

--- JavaScript/10 Days of Javascript/10DaysofJavaScript.js
+++ JavaScript/10 Days of Javascript/10DaysofJavaScript.js
@@ -7,3 +7,18 @@ function greeting(parameterVariable) {
   // Write a line of code that prints parameterVariable to stdout using console.log:
   console.log(parameterVariable);
 }
+
+/************** Day 1: Arithmetic Operators **************/
+function getArea(length, width) {
+  let area;
+  // Write your code here
+  area=length*width;
+  return area;
+}
+
+function getPerimeter(length, width) {
+  let perimeter;
+  // Write your code here
+  perimeter=2*(length+width);
+  return perimeter;
+}
