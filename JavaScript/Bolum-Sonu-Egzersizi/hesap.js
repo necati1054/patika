751c095bfb40ad8bd5b9c6f8a13bc486863222d6
Necati <necati.arman1054@gmail.com>
2022-02-10 15:59:54 +0300

    bolum sonu + clock add finish

---
+++ JavaScript/Bolum-Sonu-Egzersizi/hesap.js
@@ -0,0 +1,53 @@
+let bilgi = prompt("Not Giriniz 0-100")
+let s;
+const smile=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+<path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg>`
+const sad = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">
+<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+<path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg>`
+if(bilgi>=0 && bilgi<=100){
+    if(bilgi>=90){
+        s="AA";
+    }
+    else if(bilgi>=85){
+        s="BA";
+    }
+    else if(bilgi>=80){
+       s="BB";
+   }
+   else if(bilgi>=75){
+       s="CB";
+    }
+    else if(bilgi>=70){
+        s="CC";
+    }
+    else if(bilgi>=65){
+        s="DC";
+    }
+    else if(bilgi>=60){
+        s="DD";
+    }
+    else if(bilgi>=50){
+        s="FD";
+    }
+    else if(bilgi<=49){
+        s="FF";
+    }
+     
+    if(s==="FF"){
+    document.getElementById("info").classList.add('text-danger');
+    document.getElementById("info").innerHTML=sad+" "+s+" --> "+ bilgi
+    }
+    else{        
+    document.getElementById("info").classList.add('text-primary');
+    document.getElementById("info").innerHTML=smile+" "+s+" --> "+ bilgi
+    }
+}
+
+
+else{
+    document.body.innerHTML="AQ 100 den büyük yada küçük not mu geri zekalı";
+}
---
+++ JavaScript/Karsılama-ekrani/js/clock.js
@@ -0,0 +1,24 @@
+const myName = document.querySelector("#myName")
+const Clock = document.querySelector("#myClock")
+const günler = ["pazartesi", "salı", "çarşamba", "perşembe", "cuma", "cumartesi", "pazar"]
+let Name = prompt("Lütfen isminizi giriniz", "guest");
+if (Name.length > 0) {
+    myName.innerHTML = Name;
+}
+
+function zaman(){
+    let tarih = new Date();
+    let gün = tarih.getDay();
+    let saat = tarih.getHours();
+    let dakika = tarih.getMinutes();
+    let saniye = tarih.getSeconds();
+    let anlık = saat + ":" + dakika + ":" + saniye + " " + günler[gün];
+    return anlık;
+}
+
+function showTime(){
+    Clock.innerHTML=zaman();    
+    setTimeout(showTime,1000);
+}
+
+
