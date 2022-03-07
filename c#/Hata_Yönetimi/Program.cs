3dbdf264ec510483f78e8238c06c691c0e2837b0
Necati <necati.arman1054@gmail.com>
2022-03-07 23:39:41 +0300

    Hata yönetimi Finish

---
+++ c#/Hata_Yönetimi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// try
+// {
+// Console.WriteLine("bir sayı giriniz");
+// int sayı = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("sayı:"+ sayı);
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine("hata:"+ex.Message.ToString());
+// }
+// finally
+// {
+//     Console.WriteLine("İşlem tamamlandı");
+// }
+
+
+try
+{
+    // int a = int.Parse(null);
+    // int a = int.Parse("test");
+    int a = int.Parse("-200000000000");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("boş değer dirdiniz!");
+    Console.WriteLine(ex);
+}
+catch(FormatException ex){
+    Console.WriteLine("veri tipi uygun değil!");
+    Console.WriteLine(ex);
+}
+catch(OverflowException ex){
+    Console.WriteLine("çok küyük yada çok büyük bir değer girdiniz!");
+    Console.WriteLine(ex);
+}
