5575814a6c55ca555d0d5b281a9a86f7c7fdf515
Necati <necati.arman1054@gmail.com>
2022-03-08 00:00:50 +0300

    if-else finish

---
+++ c#/if_else/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+int time = DateTime.Now.Hour;
+if(time>=6 && time<11)
+{
+    Console.WriteLine("Günaydın");
+} 
+else if(time<=18)
+{
+    Console.WriteLine("iyi günler"); 
+}
+else
+{
+    Console.WriteLine("iyi geceler");
+}
+
+string sonuc = time<=18 ? "iyi günler" : "iyi geceler";
+
+sonuc = time>=6 && time<11 ? "Günaydın" : time<=18 ? "iyi günler" : "iyi geceler";
+
+Console.WriteLine(sonuc);
