21ef9b90465e24f284ac79da085a70820fe2d68e
Necati <necati.arman1054@gmail.com>
2022-03-08 19:54:46 +0300

    Donguler_while_foreach

---
+++ c#/Donguler_while_foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("******While******");
+Console.WriteLine("lütfen bir sayı giriniz");
+int s = int.Parse(Console.ReadLine());
+int sayac = 1;
+int t=0;
+while (sayac<=s)
+{
+    t+=sayac;
+    sayac++;
+}
+Console.WriteLine("ort:"+t/s);
+
+// a-z arasını yazdır.
+char k = 'a';
+while(k<='z')
+{
+    Console.Write(k);
+    k++;
+}
+
+
+Console.WriteLine("*******foreach******");
+string[] arabalar = {"bmw","ford","toyota","nissan"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
