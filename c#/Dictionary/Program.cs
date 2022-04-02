53f11104341e0ab04d3b941626060460683022b3
Necati <necati.arman1054@gmail.com>
2022-04-02 18:17:04 +0300

    Dictionary

---
+++ c#/Dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+//keyler tekil olmalı
+
+Dictionary<int,string> kullanıcılar = new Dictionary<int, string>();
+
+kullanıcılar.Add(10,"Necati");
+kullanıcılar.Add(11,"Ayşe");
+kullanıcılar.Add(18,"deniz");
+kullanıcılar.Add(20,"özcan");
+
+//dizinin elemanlarına erişim 
+Console.WriteLine("****** Elemanlara Erişim ******");
+Console.WriteLine(kullanıcılar[10]);
+
+foreach (var item in kullanıcılar)
+{
+    Console.WriteLine(item);
+}
+
+//count
+Console.WriteLine("****** Count ******");
+Console.WriteLine(kullanıcılar.Count);
+
+//Contains
+Console.WriteLine("****** Contains ******");
+Console.WriteLine(kullanıcılar.ContainsKey(10));
+Console.WriteLine(kullanıcılar.ContainsValue("necati arman"));
+
+//remove
+Console.WriteLine("****** Remove ******");
+kullanıcılar.Remove(20);
+foreach (var item in kullanıcılar)
+{
+    Console.WriteLine(item);
+}
+
+//Keys 
+Console.WriteLine("****** Keys ******");
+foreach (var item in kullanıcılar.Keys)
+{
+    Console.WriteLine(item);
+}
+
+//Values
+Console.WriteLine("****** Values ******");
+foreach (var item in kullanıcılar.Values)
+{
+    Console.WriteLine(item);
+}
